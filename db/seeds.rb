2579840a567cd282cc4ab61b2848ef9765df86dc
camilo cuervo <camilo267@hotmail.es>
2020-05-07 18:14:33 +1000

    cocktail app working (no styles)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts 'Creating Ingredients'
+ingredients = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+ingredients["drinks"].each do |ele|
+    Ingredient.create(name:ele["strIngredient1"])
+end
+puts "Done making Ingredients"
